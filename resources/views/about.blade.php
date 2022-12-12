8471896c76f9cd7b5042de71d22a0c79216c25ce
Deviindriawati02 <203040039@mail.unpas.ac.id>
2022-12-12 14:30:13 +0700

    Menyelesaikan Halaman About

---
+++ resources/views/about.blade.php
@@ -0,0 +1,70 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Web Slebew</title>
+    <link rel="stylesheet" href="../../css/style.css">
+    <link rel="icon" href="../../img/LogoNotFound.png">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+</head>
+
+<body>
+    <div class="container">
+        <nav class="navbar navbar-expand-sm ">
+            <a class="navbar-brand" href=""><img src="../../img/LogoNotFound.png" alt=""
+                    style="width: 180px;"></a>
+            <div class="container-fluid">
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
+                    data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
+                    aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                    <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                        <li class="nav-item mx-5">
+                            <a class="nav-link" aria-current="page" href="#">About</a>
+                        </li>
+                        <li class="nav-item mx-5">
+                            <a class="nav-link" href="#">Home</a>
+                        </li>
+                        <li class="nav-item2 mx-5">
+                            <a class="nav-link" aria-current="page" href="#">Category</a>
+                        </li>
+                        <li class="nav-item2 mx-5">
+                            <a class="nav-link" aria-current="page" href="#">Login/Register</a>
+                        </li>
+                    </ul>
+                    <form class="d-flex" role="search">
+                        <input class="form-control me-2" type="search" placeholder="Search Movie" aria-label="Search">
+                        <button type="button" class="btn btn-warning rounded-pill">Warning</button>
+                    </form>
+                </div>
+            </div>
+        </nav>
+        <img class="trapesium" ; src="../../img/1.jpg" alt="">
+        <div class="p my-5">
+            <h1>Do You Want To Know <br> Movie</h1>
+            <br>
+            <h4>Pusing cari website yang memberikan informasi <br>
+                menarik tentang movie yang paling lengkap?<br>
+                Do You Want To Know Movie solusinya</h4>
+            <br>
+            <h5>Website yang memberikan segala informasi
+                tentang <br> movie terbaru maupun lawas</h5>
+        </div>
+
+        <button type="button" class="btn btn-warning rounded-pill btn-lg" style="width: 250px"><span
+                class="spnB">Read
+                More</span></button>
+    </div>
+
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous">
+    </script>
+</body>
+
+</html>
