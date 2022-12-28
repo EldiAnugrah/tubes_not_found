2888f6e15042c79c90de2f5a743a498c63954a3b
EldiAnugrah <203040031@mail.unpas.ac.id>
2022-12-28 16:14:23 +0700

    menambahkan halaman about

---
+++ resources/views/about.blade.php
@@ -0,0 +1,39 @@
+@extends('layouts.main')
+
+@section('content')
+
+    <div class="container px-4 text-center">
+        <div class="row gx-5">
+            <div class="col-md-4 mb-3">
+                <div class="card">
+                    <div class="imgBx">
+                        <img src="img/1.jpeg" alt="images" />
+                    </div>
+                    <div class="details">
+                        <h2>Project Manager<br /><span>Eldi Anugrah</span></h2>
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-md-4 mb-3">
+                <div class="card">
+                    <div class="imgBx">
+                        <img src="img/1.jpeg" alt="images" />
+                    </div>
+                    <div class="details">
+                        <h2>Project Manager<br /><span>Eldi Anugrah</span></h2>
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-md-4 mb-3">
+                <div class="card">
+                    <div class="imgBx">
+                        <img src="img/1.jpeg" alt="images" />
+                    </div>
+                    <div class="details">
+                        <h2>Project Manager<br /><span>Eldi Anugrah</span></h2>
+                    </div>
+                </div>
+            </div>
+        </div>
